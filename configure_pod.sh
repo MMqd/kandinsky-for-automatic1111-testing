84a767b4011e4f76d7133b75e8770ad901180c18
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-14 16:30:20 -0400

    Create configure_pod.sh

---
+++ configure_pod.sh
@@ -0,0 +1,1 @@
+apt-get update && apt-get --fix-broken install -y && apt-get upgrade -y && apt-get install -y vim tmux python3-venv libgoogle-perftools4 libtcmalloc-minimal4 && cd /workspace && adduser --disabled-password --gecos "" u && tmux
