11ac080b55070318708007d52742c0092afc5988
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-14 17:47:57 -0400

    Update configure_pod.sh

--- configure_pod.sh
+++ configure_pod.sh
@@ -6,10 +6,28 @@ cd /workspace
 adduser --disabled-password --gecos "" u
 chown u:u -R /workspace
 
-git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui
+clone_or_pull() {
+  local dir=$1
+  local repo=$2
+  local branch=$3
+  
+  if [ -d "$dir" ]; then
+    cd "$dir"
+    git pull origin "$branch"
+  else
+    git clone --depth 1 "$repo" -b "$branch"
+  fi
+}
+
+clone_or_pull "stable-diffusion-webui" \
+             "https://github.com/AUTOMATIC1111/stable-diffusion-webui" \
+             "main"
 
 cd /workspace/stable-diffusion-webui/extensions
-git clone https://github.com/MMqd/kandinsky-for-automatic1111
+
+clone_or_pull "kandinsky-for-automatic1111" \
+             "https://github.com/MMqd/kandinsky-for-automatic1111" \
+             "main"
 
 tmux kill-session -t mysession 2>/dev/null
 tmux new-session -d -s mysession 'bash -c "su u; https://github.com/MMqd/kandinsky-for-automatic1111/webui.sh --listen"'
