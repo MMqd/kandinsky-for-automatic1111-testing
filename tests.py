340d83875ff87249dbb8527538059a8805d64181
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-14 17:51:43 -0400

    Create tests.py

---
+++ tests.py
@@ -0,0 +1,17 @@
+import requests 
+
+url = 'http://localhost:7860'
+
+payload = {
+  'prompt': 'a cute puppy',
+  'steps': 20,
+  'cfg_scale': 7,
+  'override_settings': {
+    'scripts': {
+      'format': 'grid',
+      'postprocessing': 'custom_scripts/enhance.py' 
+    }
+  }
+}
+
+response = requests.post(f'{url}/sdapi/v1/txt2img', json=payload)
