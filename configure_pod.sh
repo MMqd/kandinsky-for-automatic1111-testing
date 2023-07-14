bd5bba4c37af8fa508ae15ebddd101eb1bd792f5
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-14 17:32:15 -0400

    Update configure_pod.sh

--- configure_pod.sh
+++ configure_pod.sh
@@ -1,1 +1,8 @@
-apt-get update && apt-get --fix-broken install -y && apt-get upgrade -y && apt-get install -y vim tmux python3-venv libgoogle-perftools4 libtcmalloc-minimal4 && cd /workspace && adduser --disabled-password --gecos "" u && tmux
+apt-get update
+apt-get --fix-broken install -y
+apt-get upgrade -y
+apt-get install -y vim tmux python3-venv libgoogle-perftools4 libtcmalloc-minimal4
+cd /workspace
+adduser --disabled-password --gecos "" u
+chown u:u -R /workspace
+tmux
