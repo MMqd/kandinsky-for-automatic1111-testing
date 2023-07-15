1d602f6f2cbe536983806e824670fe95e44094d4
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-15 18:13:44 -0400

    Update configure_pod.sh

--- configure_pod.sh
+++ configure_pod.sh
@@ -6,6 +6,8 @@ adduser --disabled-password --gecos "" u
 chown u:u -R /workspace
 git config --global --add safe.directory *
 
+UI_NAME="automatic"
+
 clone_or_pull() {
   local dir=$1
   local repo=$2
@@ -20,12 +22,19 @@ clone_or_pull() {
 }
 
 cd /workspace
-clone_or_pull "stable-diffusion-webui" \
-             "https://github.com/AUTOMATIC1111/stable-diffusion-webui" \
-             "master"
+
+if [ $UI_NAME == "automatic"]; then
+  clone_or_pull "stable-diffusion-webui" \
+               "https://github.com/AUTOMATIC1111/stable-diffusion-webui" \
+               "master"
+else
+  clone_or_pull "automatic" \
+               "https://github.com/vladmandic/automatic" \
+               "master"
+fi
 
 if [ -d "/workspace/stable-diffusion-webui/extensions" ]; then
-  cd /workspace/stable-diffusion-webui/extensions
+  cd /workspace/$UI_NAME/extensions
   clone_or_pull "kandinsky-for-automatic1111" \
                "https://github.com/MMqd/kandinsky-for-automatic1111" \
                "main"
@@ -34,4 +43,4 @@ else
 fi
 yy
 tmux kill-session -t mysession 2>/dev/null
-sudo -u u tmux new-session -d -s mysession 'bash -c "/workspace/stable-diffusion-webui/webui.sh --listen --skip-torch-cuda-test"'
+sudo -u u tmux new-session -d -s mysession 'bash -c "/workspace/$UI_NAME/webui.sh --listen --skip-torch-cuda-test"'
