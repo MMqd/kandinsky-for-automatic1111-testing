d5f5f2edae602ea059f5c041bac7905af6cf7e1a
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-15 15:26:23 -0400

    Update configure_pod.sh

--- configure_pod.sh
+++ configure_pod.sh
@@ -2,7 +2,6 @@ apt-get update
 apt-get --fix-broken install -y
 apt-get upgrade -y
 apt-get install -y vim tmux python3-venv libgoogle-perftools4 libtcmalloc-minimal4
-cd /workspace
 adduser --disabled-password --gecos "" u
 chown u:u -R /workspace
 git config --global --add safe.directory *
@@ -20,15 +19,17 @@ clone_or_pull() {
   fi
 }
 
+cd /workspace
 clone_or_pull "stable-diffusion-webui" \
              "https://github.com/AUTOMATIC1111/stable-diffusion-webui" \
              "main"
 
 cd /workspace/stable-diffusion-webui/extensions
 
-clone_or_pull "kandinsky-for-automatic1111" \
-             "https://github.com/MMqd/kandinsky-for-automatic1111" \
-             "main"
+if [ -d "/workspace/stable-diffusion-webui/extensions" ]; then
+  clone_or_pull "kandinsky-for-automatic1111" \
+               "https://github.com/MMqd/kandinsky-for-automatic1111" \
+               "main"
 
 tmux kill-session -t mysession 2>/dev/null
 tmux new-session -d -s mysession 'bash -c "su u; /workspace/stable-diffusion-webui/webui.sh --listen --skip-torch-cuda-test"'
