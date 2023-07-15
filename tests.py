591f7d9fd0fe5bf8215c3b1a5f75a14e76c67892
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-15 19:19:18 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,7 +9,6 @@ api = webuiapi.WebUIApi(host='127.0.0.1', port=7860)
 
 result1 = api.txt2img(prompt="cute squirrel",
                     negative_prompt="ugly, out of frame",
-                    seed=1003,
-                    styles=["anime"],
+                    seed=0,
                     cfg_scale=7,
                     )
