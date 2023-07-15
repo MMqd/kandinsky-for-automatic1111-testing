548d95f554bf02ca752ba6e98c784bce35683eac
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-15 19:23:43 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -13,3 +13,89 @@ result1 = api.txt2img(prompt="cute squirrel",
                     save_images=True,
                     cfg_scale=7,
                     )
+
+# Available Axis options (Different for txt2img and img2img!)
+XYZPlotAvailableTxt2ImgScripts = [
+    "Nothing",
+    "Seed",
+    "Var. seed",
+    "Var. strength",
+    "Steps",
+    "Hires steps",
+    "CFG Scale",
+    "Prompt S/R",
+    "Prompt order",
+    "Sampler",
+    "Checkpoint name",
+    "Sigma Churn",
+    "Sigma min",
+    "Sigma max",
+    "Sigma noise",
+    "Eta",
+    "Clip skip",
+    "Denoising",
+    "Hires upscaler",
+    "VAE",
+    "Styles",
+]
+
+XYZPlotAvailableImg2ImgScripts = [
+    "Nothing",
+    "Seed",
+    "Var. seed",
+    "Var. strength",
+    "Steps",
+    "CFG Scale",
+    "Image CFG Scale",
+    "Prompt S/R",
+    "Prompt order",
+    "Sampler",
+    "Checkpoint name",
+    "Sigma Churn",
+    "Sigma min",
+    "Sigma max",
+    "Sigma noise",
+    "Eta",
+    "Clip skip",
+    "Denoising",
+    "Cond. Image Mask Weight",
+    "VAE",
+    "Styles",
+]
+
+# Example call
+XAxisType = "Steps"
+XAxisValues = "20,30" 
+YAxisType = "Sampler"
+YAxisValues = "Euler a, LMS"
+ZAxisType = "Nothing"
+ZAxisValues = ""
+drawLegend = "True"
+includeLoneImages = "False"
+includeSubGrids = "False"
+noFixedSeeds = "False"
+marginSize = 0
+
+
+# x_type, x_values, y_type, y_values, z_type, z_values, draw_legend, include_lone_images, include_sub_grids, no_fixed_seeds, margin_size
+
+result = api.txt2img(
+                    prompt="cute squirrel",
+                    negative_prompt="ugly, out of frame",
+                    seed=0,
+                    save_images=True,
+                    cfg_scale=7,
+                    script_name="X/Y/Z Plot",
+                    script_args=[
+                        XYZPlotAvailableTxt2ImgScripts.index(XAxisType),
+                        XAxisValues,
+                        XYZPlotAvailableTxt2ImgScripts.index(YAxisType),
+                        YAxisValues,
+                        XYZPlotAvailableTxt2ImgScripts.index(ZAxisType),
+                        ZAxisValues,
+                        drawLegend,
+                        includeLoneImages,
+                        includeSubGrids,
+                        noFixedSeeds,
+                        marginSize,                        ]
+                    )
