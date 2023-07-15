02a5adbf69e1fc2a01b7bc837c8942ce79688fff
MMqd <85073526+MMqd@users.noreply.github.com>
2023-07-15 19:11:32 -0400

    Update tests.py

--- tests.py
+++ tests.py
@@ -6,12 +6,12 @@ payload = {
   'prompt': 'a cute puppy',
   'steps': 20,
   'cfg_scale': 7#,
-  #'override_settings': {
-  #  'scripts': {
-  #    'format': 'grid',
-  #    'postprocessing': 'custom_scripts/enhance.py' 
-  #  }
-  #}
+  'override_settings': {
+    'scripts': {
+      'format': 'grid',
+      'postprocessing': 'custom_scripts/enhance.py' 
+    }
+  }
 }
 
 response = requests.post(f'{url}/sdapi/v1/txt2img', json=payload)
